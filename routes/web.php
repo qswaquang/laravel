2a91fda9364af9bd943d0317712e2f173b0af79d
qswaquang <qswaquang@gmail.com>
2022-11-29 22:43:38 +0700

    testRoute

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function(){
+    return "asdasd"
+})
+
 Route::get('/dashboard', function(){
     return view('dashboard');
 })->middleware('auth');
